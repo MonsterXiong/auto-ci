4dbbeacafa07d8dc4acb44a175c51be38dbaefc1
MonsterXiong <942849672@qq.com>
2022-03-24 19:16:53 +0800

    WIP: feat: 555

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+        es2021: true,
+        node: true,
+        jest: true,
+        es6: true,
+        commonjs: true,
+    },
+    extends: ['airbnb-base', 'plugin:prettier/recommended'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    plugins: ['prettier'],
+    rules: {
+        // 'no-unused-vars': 0,
+        'no-console': 'off',
+        'prettier/prettier': 'off',
+    },
+}
